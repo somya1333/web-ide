9cb480d627ede8c892fc71a8f2fa9fe414171688
anant <anantrungta1999@gmail.com>
2019-05-22 19:32:46 +0530

    MR

--- name/src/components/Navbar.js
+++ name/src/components/Navbar.js
@@ -16,23 +16,21 @@ class Navbar extends Component {
         <nav className="navbar navbar-expand-sm navbar-dark  mb-4" style={{backgroundColor:"black"}}>
     <div className="container mb-2 mt-1">
       <Link className="navbar-brand" to="/"><b>IOSD IDE</b></Link>
-      <button value="toggle" className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+      {/* <button value="toggle" className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
         <span className="navbar-toggler-icon"></span>
-      </button>
+      </button> */}
 
-      <div className="collapse navbar-collapse" id="mobile-nav">
+      <div className="" id="mobile-nav">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
             <Link className="nav-link" to="/php"> PHP
             </Link>
           </li>
-          <li>
-          <Link className="nav-link" to="/python"> PYTHON
-          </Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/python"> PYTHON
+            </Link>
           </li>
-     
         </ul>
-
       </div>
     </div>
   </nav>
